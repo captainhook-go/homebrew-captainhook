0236d49a6aeb036fe59a6b6aaa93d1f46a1acca5
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2023-11-22 15:56:36 +0100

    Brew formula update for captainhook version 0.9.12

--- Formula/captainhook.rb
+++ Formula/captainhook.rb
@@ -5,7 +5,7 @@
 class Captainhook < Formula
   desc "CaptainHook is a flexible git hooks manager that makes sharing git hooks a breeze"
   homepage "http://captainhook.info/"
-  version "0.9.11"
+  version "0.9.12"
   license "MIT"
 
   depends_on "git"
@@ -13,38 +13,42 @@ class Captainhook < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/captainhook-go/captainhook/releases/download/0.9.11/captainhook_Darwin_arm64.tar.gz"
-      sha256 "f5c18136b97b4e97f86ae205e056897ead8d49cbc3f459aab5fa13a560e0cfc2"
+      url "https://github.com/captainhook-go/captainhook/releases/download/0.9.12/captainhook_Darwin_arm64.tar.gz"
+      sha256 "0c6121418ac11fd7738e2d0cb5e9284024c02df745b3f6727e8363cda6e2def3"
 
       def install
         bin.install "captainhook"
+        ln_s captainhook, "#{HOMEBREW_PREFIX}/bin/git-ch", :force => true
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/captainhook-go/captainhook/releases/download/0.9.11/captainhook_Darwin_x86_64.tar.gz"
-      sha256 "2f661de65fee4cb1f29de461ecfbc7d6745b50ef87bb46efdb883b25e02e3bf5"
+      url "https://github.com/captainhook-go/captainhook/releases/download/0.9.12/captainhook_Darwin_x86_64.tar.gz"
+      sha256 "cc31ddfe96bb6f4f660c761352d7da30327cce8757a38e9882352ea6b2f51963"
 
       def install
         bin.install "captainhook"
+        ln_s captainhook, "#{HOMEBREW_PREFIX}/bin/git-ch", :force => true
       end
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/captainhook-go/captainhook/releases/download/0.9.11/captainhook_Linux_arm64.tar.gz"
-      sha256 "fe897d2a6857b0a9d046e55a6a14af174865f1bafe005fc6464df15cc9b24a74"
+      url "https://github.com/captainhook-go/captainhook/releases/download/0.9.12/captainhook_Linux_arm64.tar.gz"
+      sha256 "d3f96fdab3a8d00816a4b910dea07b7fb22ca923c5fc09f9beed45047b953221"
 
       def install
         bin.install "captainhook"
+        ln_s captainhook, "#{HOMEBREW_PREFIX}/bin/git-ch", :force => true
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/captainhook-go/captainhook/releases/download/0.9.11/captainhook_Linux_x86_64.tar.gz"
-      sha256 "0f6189b9bf8254b84b001957e1edf68b59d58d937f35ab62adde3193b714c381"
+      url "https://github.com/captainhook-go/captainhook/releases/download/0.9.12/captainhook_Linux_x86_64.tar.gz"
+      sha256 "9560b78e1a63e8493b07aa58209613788a5504e163f1e5050dd663dfb581f96e"
 
       def install
         bin.install "captainhook"
+        ln_s captainhook, "#{HOMEBREW_PREFIX}/bin/git-ch", :force => true
       end
     end
   end
